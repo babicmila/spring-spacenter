a25de25848fdcde59cf03d95d52f8a6b1424d84b
Mila Babic <milababic5@gmail.com>
2021-02-17 21:23:09 +0100

    initial commit

---
+++ src/main/java/rs/ac/metropolitan/spacenter/SpacenterApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.metropolitan.spacenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/metropolitan/spacenter/SpacenterApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.metropolitan.spacenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
